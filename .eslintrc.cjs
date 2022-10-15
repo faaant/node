33260be62492d9b31589d88a68d98a850c05046a
Illia Adamchuk <illia.adamchuk.work@gmail.com>
2022-10-15 17:29:01 +0300

    add prettier, config for lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,14 @@
 module.exports = {
   env: {
     node: true,
-    es2021: true
+    es2021: true,
   },
-  plugins: ['sonar'],
-  extends: ['standard','plugin:sonar/recommended'],
-  overrides: [
-  ],
+  plugins: ["sonar"],
+  extends: ["standard", "plugin:sonar/recommended", "prettier"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  rules: {
-  }
+  rules: {},
 };
